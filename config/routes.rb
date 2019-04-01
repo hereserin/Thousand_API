fadfe74f47642317e7481fd11621f704a1c66b83
Erin Marshall <erin.e.mars@gmail.com>
2019-04-01 15:55:15 -0400

    Set up api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ require 'api_constraints'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults: {format: :json}, constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1 do
-      resources :pages, only: [:index]
-    end
+
+  namespace :api, defaults: {format: :json} do
+    resources :pages, only: [:index]
   end
+
 
 
 
